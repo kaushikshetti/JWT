ebb5ad52d7ca5e90500ab96333c8ceec4d482b87
Kaushik Shetti <kshetti29@gmail.com>
2023-09-23 00:23:12 +0530

    Signup text added

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,22 @@
 const router = require("express").Router();
-const berypt = require("beryptjs");
+const bcrypt = require("bcryptjs");
 
 const jwt = require("jsonwebtoken");
 
 const User = require("../model/user");
 
 //Signup users
+router.post("/signup",async(req,res)=>{
+    const salt=await bcrypt.genSalt(10);
+    const hash=await bcrypt.hash(req.body.password,salt);
 
+    const user=new User({
+          username:req.body.username,
+          password:hash
+    })
+
+    const saveUser=await user.save()
+    res.status(200).send("User created");
+})
 
 module.exports = router;

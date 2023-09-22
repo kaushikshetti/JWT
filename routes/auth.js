889f3308a59d017c282b1c71bfddd074108154a1
Kaushik Shetti <kshetti29@gmail.com>
2023-09-23 00:02:41 +0530

    Progress (Model + Routes)

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const berypt = require("beryptjs");
+
+const jwt = require("jsonwebtoken");
+
+const User = require("../model/user");
+
+//Signup users
+
+
+module.exports = router;
